8f6726d14203ba8bb7c30d22c8c8e2ee64c354fd
multiheros <jonathannc41@gmail.com>
2021-12-04 16:09:36 -0300

    Criado a pasta para testes do projeto

---
+++ test/test.py
@@ -0,0 +1,9 @@
+from convertTo import convertTo
+
+def main():
+    test = convertTo()
+    test.__convertInput__()
+    test.__convertMachine__()
+    test.__makeTape__()
+    test.__print__()
+main()
